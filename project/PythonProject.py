8da5a1abf1ecc94fb20be4f3b035620055b5c971
BlessingEmmanuel321 <122313591+BlessingEmmanuel321@users.noreply.github.com>
2023-04-28 19:25:53 +0100

    Update PythonProject.py

--- project/PythonProject.py
+++ project/PythonProject.py
@@ -64,220 +64,263 @@ class PersonalDetails:
 
     def password_encrypted(self):
         return hashlib.sha256(self.password.encode()).hexdigest()
+    
+class CodeWordGame:
+    def __init__(self) -> None:
+        
 
 
-admin_name = "Blessing"
-admin_username, admin_password = username_password(admin_name)
-admin_info = PersonalDetails(admin_name, admin_username, admin_password)
+        self.admin_name = "Blessing"
+        self.admin_username, self.admin_password = username_password(self.admin_name)
+        self.admin_info = PersonalDetails(self.admin_name, self.admin_username, self.admin_password)
 
-print("Lets create your login details.")
-name1 = input("\nEnter player one's name: ")
-username1, password1 = username_password(name1)
-player1 = PersonalDetails(name1, username1, password1)
+        print("Lets create your login details.")
+        self.name1 = input("\nEnter player one's name: ")
+        self.username1, self.password1 = username_password(self.name1)
+        self.player1 = PersonalDetails(self.name1, self.username1, self.password1)
 
-name2 = input("Enter player two's name: ")
-username2, password2 = username_password(name2)
-player2 = PersonalDetails(name2, username2, password2)
+        self.name2 = input("Enter player two's name: ")
+        self.username2, self.password2 = username_password(self.name2)
+        self.player2 = PersonalDetails(self.name2, self.username2, self.password2)
 
-print(f"\n{player1.name}'s login info:\nUsername: {player1.username}\nPassword: {player1.password}\n")
-print(f"{player2.name}'s login info:\nUsername: {player2.username}\nPassword: {player2.password}")
+        print(f"\n{self.player1.name}'s login info:\nUsername: {self.player1.username}\nPassword: {self.player1.password}\n")
+        print(f"{self.player2.name}'s login info:\nUsername: {self.player2.username}\nPassword: {self.player2.password}")
 
-players_details = [[admin_info.name, admin_info.username_encrypted(), admin_info.password_encrypted()],
-                   [player1.name, player1.username_encrypted(), player1.password_encrypted(), player1.health_value],
-                   [player2.name, player2.username_encrypted(), player2.password_encrypted(), player2.health_value]]
+        self.players_details = [[self.admin_info.name, self.admin_info.username_encrypted(), self.admin_info.password_encrypted()],
+                        [self.player1.name, self.player1.username_encrypted(), self.player1.password_encrypted(), self.player1.health_value],
+                        [self.player2.name, self.player2.username_encrypted(), self.player2.password_encrypted(), self.player2.health_value]]
 
 
-def start_program():
-    print("\n\nMain Menu")
-    print("\n1: Administrator")
-    print("2: Player")
-    print("3: Exit the system")
-    choice = int(input("\nChoose an option: "))
-    if choice == 1:
-        if login(admin_info.username_encrypted(), admin_info.password_encrypted()):
-            print("You have successfully logged in as the administrator.")
-        else:
-            print("Incorrect username or password")
-    elif choice == 2:
-        for i in range(2):
-            name = input("Enter your name: ")
-            for PLAYER_DETAILS in players_details[1:]:
-                if name == PLAYER_DETAILS[0]:
-                    if login(PLAYER_DETAILS[1], PLAYER_DETAILS[2]):
-                        print(f"{name} has successfully logged in")
-                        break
-                    else:
-                        print("Incorrect username or password")
-                        break
+    def start_program(self):
+        print("\n\nMain Menu")
+        print("\n1: Administrator")
+        print("2: Player")
+        print("3: Exit the system")
+        choice = int(input("\nChoose an option: "))
+        if choice == 1:
+            if self.login(self.admin_info.username_encrypted(), self.admin_info.password_encrypted()):
+                print("You have successfully logged in as the administrator.")
             else:
-                print("\nNo player found with that name\n")
-        player_menu()
-    elif choice == 3:
-        exit()
-    else:
-        print("Invalid choice")
+                print("Incorrect username or password")
+        elif choice == 2:
+            for i in range(2):
+                name = input("Enter your name: ")
+                for player_detail in self.players_details[1:]:
+                    if name == player_detail[0]:
+                        if self.login(player_detail[1], player_detail[2]):
+                            print(f"{name} has successfully logged in")
+                            break
+                        else:
+                            print("Incorrect username or password")
+                            break
+                else:
+                    print("\nNo player found with that name\n")
+            self.player_menu()
+        elif choice == 3:
+            exit()
+        else:
+            print("Invalid choice")
 
 
-def login(username_encrypted, password_encrypted):
-    logged_in = False
-    count = 3
-    while (not logged_in) and (count > 0):
-        username_attempt = input("Enter your username: ")
-        if username_encrypted == hashlib.sha256(username_attempt.encode()).hexdigest():
-            password_attempt = input("Enter your password: ")
-            if password_encrypted == hashlib.sha256(password_attempt.encode()).hexdigest():
-                logged_in = True
-                break
+    def login(self, username_encrypted, password_encrypted):
+        logged_in = False
+        count = 3
+        while (not logged_in) and (count > 0):
+            username_attempt = input("Enter your username: ")
+            if username_encrypted == hashlib.sha256(username_attempt.encode()).hexdigest():
+                password_attempt = input("Enter your password: ")
+                if password_encrypted == hashlib.sha256(password_attempt.encode()).hexdigest():
+                    logged_in = True
+                    break
+                else:
+                    print("Incorrect password. Try again")
             else:
-                print("Incorrect password. Try again")
-        else:
-            print("Incorrect username. Try again")
-        count -= 1
+                print("Incorrect username. Try again")
+            count -= 1
 
-    return logged_in
+        return logged_in
 
 
-def admin_menu():
-    print("1: Display codeword")
-    print("2: Display incomplete codeword")
-    print("3: Display key grid")
-    print("4: Display incomplete key grid")
-    print("5: Add a different word to the puzzle")
-    print("6: Delete a word from the puzzle")
-    print("7: ")
-    print("8: ")
-    print("9: ")
-    menu_choice = input("Choose an option: ")
-    if menu_choice not in range(1, 10):
-        print(menu_choice)
-    if menu_choice == 1:
-        display_codeword()
-    elif menu_choice == 2:
-        display_player_codeword()
-    elif menu_choice == 3:
-        display_answer_key()
-    elif menu_choice == 4:
-        display_player_key()
+    def admin_menu(self):
+        print("1: Display codeword")
+        print("2: Display incomplete codeword")
+        print("3: Display key grid")
+        print("4: Display incomplete key grid")
+        print("5: Add a different word to the puzzle")
+        print("6: Delete a word from the puzzle")
+        print("7: ")
+        print("8: ")
+        print("9: ")
+        menu_choice = input("Choose an option: ")
+        if menu_choice not in range(1, 10):
+            print(menu_choice)
+        if menu_choice == 1:
+            self.display_codeword()
+        elif menu_choice == 2:
+            self.display_player_codeword()
+        elif menu_choice == 3:
+            self.display_answer_key()
+        elif menu_choice == 4:
+            self.display_player_key()
 
 
-def display_codeword():
-    for row in codeword:
-        for column in row:
-            print(column, end=" ")
-        print(" ")
-
-
-def display_player_codeword():
-    for row in player_codeword:
-        for column in row:
-            print(column, end=" ")
-        print(" ")
-
-
-def display_answer_key():
-    for row in answer_key:
-        for column in row:
-            print(column, end=" ")
-        print(" ")
-
-
-def display_player_key():
-    for row in player_key:
-        for column in row:
-            print(column, end=" ")
-        print(" ")
-
-
-def player_menu():
-    print("1: Play the Game")
-    print("2: Display the statistics")
-    choice = int(input("Choose an option: "))
-    if choice == 1:
-        player_check(player1.health_value, player2.health_value)
-
-
-def player_check(health1, health2):
-    player1_answer = input("Player 1, do you want to continue playing? (y/n): ")
-    player2_answer = input("Player 2, do you want to continue playing? (y/n): ")
-
-    if player1_answer.lower() == 'y' and player2_answer.lower() == 'y':
-        if health1 >= 2 and health2 >= 2:
-            print("Both players can continue playing.")
-            game_rules()
-        else:
-            print("You don't have enough lives.")
-    else:
-        print("Game ended.")
-
-
-def game_rules():
-    print("\nWelcome to Codeword Games!")
-    print("\nHere are the rules:")
-    print("1. You must guess the letters in the codeword.")
-    print("2. You have 5 chances to guess correctly.")
-    print("3. For each incorrect guess, you will lose 2 health values.")
-    print("4. If you lose all of your health values, the game is over.")
-    print("5. The first player to guess the entire codeword wins the game.")
-    print("\nLet's begin!")
-    print("\nHere is the Codeword:")
-    for row in player_codeword:
-        for column in row:
-            print(column, end=" ")
-        print(" ")
-    for key in player_key:
-        for num in key:
-            print(num, end=" ")
-        print(" ")
-    play_game()
-
-
-def play_game():
-
-    correct_guesses = 0
-    incorrect_guesses = 0
-
-    while correct_guesses < 15:
-        player_check(health1, health2)
-        player_num = int(input("Enter your player number (1 or 2): "))
-        player_name = ""
-        for player in players_details[1:]:
-            if player_num == 1 and player[0] == player1.name:
-                player_name = player1.name
-                health1 = player1.health_value
-                health1 -= (incorrect_guesses*2)
-                health1 += (correct_guesses*2)
-                break
-            elif player_num == 2 and player[0] == player2.name:
-                player_name = player2.name
-                health2 = player2.health_value
-                health2 -= (incorrect_guesses*2)
-                health2 += (correct_guesses*2)
-                break
-
-        cell = input("Enter the number you want to guess for: ")
-        while int(cell) not in range(1, 16):
-            print(cell)
-        letter_guess = input("Enter the letter that you want to guess for: ")
-        matched_cells = []
-        for row in range(len(player_codeword)):
-            for col in range(len(player_codeword[row])):
-                if str(cell) in str(player_codeword[row][col]):
-                    player_codeword[row][col] = letter_guess
-                    matched_cells.append((row, col))
-        for x in player_codeword:
-            for y in x:
-                print(y, end=" ")
+    def display_codeword(self):
+        for row in codeword:
+            for column in row:
+                print(column, end=" ")
             print(" ")
-        if letter_guess in codeword:
-            print("Correct answer!")
-            correct_guesses += 1
-            if codeword == player_codeword:
-                print(f"{player_name}, has successfully won")
+
+
+    def display_player_codeword():
+        for row in player_codeword:
+            for column in row:
+                print(column, end=" ")
+            print(" ")
+
+
+    def display_answer_key():
+        for row in answer_key:
+            for column in row:
+                print(column, end=" ")
+            print(" ")
+
+
+    def display_player_key():
+        for row in player_key:
+            for column in row:
+                print(column, end=" ")
+            print(" ")
+
+
+    def player_menu(self):
+        print("1: Play the Game")
+        print("2: Display the statistics")
+        choice = int(input("Choose an option: "))
+        if choice == 1:
+            self.health1 = self.player1.health_value
+            self.health2 = self.player2.health_value
+            self.player_check()
+
+
+    def player_check(self):
+        player1_answer = input("Player 1, do you want to continue playing? (y/n): ")
+        player2_answer = input("Player 2, do you want to continue playing? (y/n): ")
+
+        if player1_answer.lower() == 'y' and player2_answer.lower() == 'y':
+            if self.health1 >= 2 and self.health2 >= 2:
+                print("Both players can continue playing.")
+                self.game_rules()
+            else:
+                print("You don't have enough lives.")
         else:
-            print(f"{player_name}, your guess was incorrect therefore you lose 2 health points")
-            for row, col in matched_cells:
-                player_codeword[row][col] = str(cell)
-            incorrect_guesses += 1
+            print("Game ended.")
 
 
-start_program()
+    def game_rules(self):
+        print("\nWelcome to Codeword Games!")
+        print("\nHere are the rules:")
+        print("1. You must guess the letters in the codeword.")
+        print("2. You have 5 chances to guess correctly.")
+        print("3. For each incorrect guess, you will lose 2 health values.")
+        print("4. If you lose all of your health values, the game is over.")
+        print("5. The first player to guess the entire codeword wins the game.")
+        print("\nLet's begin!")
+        print("\nHere is the Codeword:")
+        for row in player_codeword:
+            for column in row:
+                print(column, end=" ")
+            print(" ")
+        for key in player_key:
+            for num in key:
+                print(num, end=" ")
+            print(" ")
+        self.play_game()
+
+
+    def play_game(self):
+
+        self.correct_guesses_p1 = 0
+        self.correct_guesses_p2 = 0
+        self.incorrect_guesses_p1 = 0
+        self.incorrect_guesses_p2 = 0
+        player_num_flag  = False
+        while self.correct_guesses_p1 < 15 and self.correct_guesses_p2 < 15 and self.health1> 2 and self.health2 > 2:
+            # player_num = int(input("Enter your player number (1 or 2): "))
+            player_name = ""
+            
+            if player_num_flag == False:
+                player_name = self.player1.name
+                print(f"""Player 1 ({player_name})....
+                YOUR UP!....
+                      """)
+            elif player_num_flag == True:
+                player_name = self.player2.name
+                print(f"""Player 2 ({player_name})....
+                IT'S YOUR TURN!....
+                      """)
+            for player in self.players_details[1:]:
+                cell = input("Enter the number you want to guess for: ")
+                while int(cell) not in range(1, 16):
+                    print(cell)
+                letter_guess = input("Enter the letter that you want to guess for: ")
+                matched_cells = []
+                for row in range(len(player_codeword)):
+                    for col in range(len(player_codeword[row])):
+                        if str(cell) == str(player_codeword[row][col]):
+                            player_codeword[row][col] = letter_guess
+                            matched_cells.append((row, col))
+                for x in player_codeword:
+                    for y in x:
+                        print(y, end=" ")
+                    print(" ")
+                
+                if codeword[matched_cells[0][0]][matched_cells[0][1]] == letter_guess: 
+                # if letter_guess in codeword:
+                    # print("Correct answer!")
+                    if player_num_flag == False:
+                        self.correct_guesses_p1 += 1
+                        self.health1 += 2
+                        print(f""" Well done Player 1 ({player_name} !!""")
+                    else:
+                        self.correct_guesses_p2 += 1
+                        self.health2 += 2
+                        print(f""" Excellent Player 2 ({player_name} !!""")
+                    if codeword == player_codeword:
+                        print(f"{player_name}, has successfully won")
+                else:
+                    print(f"{player_name}, your guess was incorrect therefore you lose 2 health points")
+                    for row, col in matched_cells:
+                        player_codeword[row][col] = str(cell)
+                    if player_num_flag == False:
+                        self.incorrect_guesses_p1 += 1
+                        self.health1 -= 2
+                        player_num_flag = True
+                        print("""Better luck next time...
+                        Time to switch it up!""")
+                    else:
+                        self.incorrect_guesses_p2 += 1
+                        self.health2 -= 2
+                        player_num_flag = False
+                        print("""Your throwing it away...
+                        Need to try harder!""")
+
+
+                    # if player_num == 1 and player[0] == self.player1.name:
+                    #     player_name = self.player1.name
+                    #     # health1 = player1.health_value
+                    #     self.health1 -= (incorrect_guesses*2)
+                    #     self.health1 += (correct_guesses*2)
+                    #     break
+                    # elif player_num == 2 and player[0] == self.player2.name:
+                    #     player_name = self.player2.name
+                    #     # health2 = player2.health_value
+                    #     self.health2 -= (incorrect_guesses*2)
+                    #     self.health2 += (correct_guesses*2)
+                    #     break
+
+
+if __name__ == '__main__':
+    game = CodeWordGame()
+    game.start_program()
+
+    
